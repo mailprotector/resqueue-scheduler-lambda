2142496cd593a01ec17517f6e90623efbdaeaee6
Dan Wagoner <danwagoner@users.noreply.github.com>
2023-01-16 15:29:41 -0500

    package script fix

--- package.sh
+++ package.sh
@@ -3,7 +3,7 @@
 set -e
 
 readonly SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-readonly VENV="resque-scheduler/venv"
+readonly VENV="resque-scheduler-lambda/.venv"
 readonly PYTHON="python3.8"
 readonly ZIP_FILE="package.zip"
 readonly SITE_PACKAGES=${VENV}/lib/${PYTHON}/site-packages
